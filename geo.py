392c319c1d5f080061786284fc7ced5768029a44
Fcovitor <vitoraquino04@gmail.com>
2018-04-09 09:19:18 -0300

    adicionando arquivo com funcoes geometricas

---
+++ geo.py
@@ -0,0 +1,20 @@
+def area_circuito(r):
+    return 3.14*(r**2)
+    
+def area_quadrado(l):
+    return l*l
+    
+def area_triangulo(b,a):
+    return (b*a)/2
+    
+def are_retangulo(b,a):
+    return b*a
+ 
+def are_trapezio(b1,b2,a):
+    return ((b1+b2)*a)/2
+    
+def are_losango(d1,d2):
+    return (d1*d2)/2
+    
+def are_pentagono(a,b):
+    return (a*p)/2            
