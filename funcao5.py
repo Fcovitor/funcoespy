402b13f9d87ce61538630e5da9fa7322a096e6ec
Fcovitor <38197938+Fcovitor@users.noreply.github.com>
2018-04-25 14:05:52 -0300

    Add files via upload

---
+++ funcao5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def questao5(a,b,c,d,e,f,g,h,i):
+    a=("só")
+    b=("trabalho")
+    c=("sem") 
+    d=("diversao")
+    e=("faz")
+    f=("de")
+    g=("joao")
+    h=("em")
+    i=("chato")
+    return ("so trabalho sem diversao faz de joao em chato")
+
+print(questao5("a","b","c","d","e","f","g","h","i"))
+
